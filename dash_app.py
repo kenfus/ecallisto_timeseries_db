35379c9695d75e199947e7c356587fb68155e77e
kenfus <vincenzo.timmel@gmail.com>
2023-06-27 12:39:38 +0000

    Added interactive zooming

--- dash_app.py
+++ dash_app.py
@@ -1,5 +1,5 @@
 import dash
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from dash import dcc, html
 import plotly.express as px
 from database_functions import timebucket_values_from_database_sql, values_from_database_sql, sql_result_to_df, fill_missing_timesteps_with_nan
@@ -12,42 +12,42 @@ app = dash.Dash(__name__)
 RESOLUTION_WIDTH = 720
 query = {
     "table": "austria_unigraz_01",
-    "start_datetime": '2021-03-10 06:30:00',
+    "start_datetime": '2021-03-01 06:30:00',
     "end_datetime": '2021-03-14 23:30:00',
-    "timebucket": "30m",
+    "timebucket": "1h",
     "agg_function": "MAX"
-} # Also the "base query"
+}  # Also the "base query"
 
-# 1 version alle und liste.
-# Todo: timezone pro instrument. Tageslänge pro lat/lon.
+# Fetch initial data
 sql = timebucket_values_from_database_sql(**query)
 df = sql_result_to_df(sql, datetime_col='datetime')
 df = fill_missing_timesteps_with_nan(df)
 
 fig = px.imshow(df.T.iloc[::-1])
 
-
 app.layout = html.Div([
     dcc.Graph(
         id='live-graph',
         figure=fig
     ),
-    html.Div(id='hidden-div', style={'display':'none'})
+    html.Div(id='hidden-div', style={'display': 'none'})
 ])
 
 @app.callback(
     Output('live-graph', 'figure'),
-    [Input('live-graph', 'relayoutData')])
-def update_graph(rangeselector):
+    [Input('live-graph', 'relayoutData')],
+    [State('live-graph', 'figure')]
+)
+def update_graph(rangeselector, current_figure):
     if rangeselector is not None:
         if 'xaxis.range[0]' in rangeselector:
-            start_datetime = rangeselector['xaxis.range[0]']
-            end_datetime = rangeselector['xaxis.range[1]']
+            start_datetime = pd.to_datetime(rangeselector['xaxis.range[0]'])
+            end_datetime = pd.to_datetime(rangeselector['xaxis.range[1]'])
+
+            print(f"Start: {start_datetime}, End: {end_datetime}")
 
             # Time difference
-            start_datetime = pd.to_datetime(start_datetime)
-            end_datetime = pd.to_datetime(end_datetime)
-            time_delta = calculate_timedelta_from_strings(start_datetime, end_datetime) / RESOLUTION_WIDTH
+            time_delta = (end_datetime - start_datetime) / RESOLUTION_WIDTH
             timebucket_value = timedelta_to_sql(time_delta)
 
             print(timebucket_value)
@@ -67,20 +67,16 @@ def update_graph(rangeselector):
             sql = timebucket_values_from_database_sql(**query)
             df = sql_result_to_df(sql, datetime_col='datetime')
             df = fill_missing_timesteps_with_nan(df)
-            print(df.head())
 
             # Update figure
-            fig = px.imshow(df.T.iloc[::-1])
-            fig.update_layout(
-                title=f"Spectogram from {start_datetime} to {end_datetime}",
-                xaxis_title="Datetime",
-                yaxis_title="Frequency",
-                font=dict(family="Courier New, monospace", size=18, color="#7f7f7f"),
-            )
+            current_figure['data'] = px.imshow(df.T.iloc[::-1]).data
 
+            return current_figure
+        
+        elif 'xaxis.autorange' in rangeselector:
             return fig
-    else:
-        return dash.no_update
+
+    return dash.no_update
 
 if __name__ == '__main__':
     app.run_server(debug=True)
