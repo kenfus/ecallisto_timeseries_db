8e66c6203e36c477d47b8f2229ac960e9d98f057
kenfus <vincenzo.timmel@gmail.com>
2023-10-20 14:57:15 +0000

    Added more to blacklsit.

--- project_config.py
+++ project_config.py
@@ -1,7 +1,7 @@
 # INSTRUMENT BLACK LIST
 # Those instrument are not added to the database
 BLACK_LIST = ["mongolia_ub_01", "roswell_nm_58", "roswell_nm_57", "norway_randaberg_01", "norway_ny_aalesund_01", "southafrica_sansa_01",
-              "spain_siguenza_02", "germany_dlr_01",
+              "spain_siguenza_02", "germany_dlr_01", "romania_01", "egypt_alexandria_01","egypt_alexandria_02", "germany_dlr_63",
 "swiss_irsol_01"
 "swiss_heiterswil_59",
 ]
