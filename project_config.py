5b20bca260cddca0986df7d7d2c44660f1039564
kenfus <vincenzo.timmel@gmail.com>
2023-10-23 07:36:00 +0000

    Updated blacklist.

--- project_config.py
+++ project_config.py
@@ -1,7 +1,19 @@
 # INSTRUMENT BLACK LIST
 # Those instrument are not added to the database
-BLACK_LIST = ["mongolia_ub_01", "roswell_nm_58", "roswell_nm_57", "norway_randaberg_01", "norway_ny_aalesund_01", "southafrica_sansa_01",
-              "spain_siguenza_02", "germany_dlr_01", "romania_01", "egypt_alexandria_01","egypt_alexandria_02", "germany_dlr_63",
-"swiss_irsol_01", "germany_dlr_62"
-"swiss_heiterswil_59",
+BLACK_LIST = [
+"mongolia_ub_01",
+"roswell_nm_58",
+"roswell_nm_57",
+"norway_randaberg_01",
+"norway_ny_aalesund_01",
+"southafrica_sansa_01",
+"spain_siguenza_02",
+"germany_dlr_01",
+"romania_01",
+"egypt_alexandria_01",
+"egypt_alexandria_02",
+"germany_dlr_63",
+"swiss_irsol_01",
+"germany_dlr_62swiss_heiterswil_59",
+"india_udaipur_02"
 ]
